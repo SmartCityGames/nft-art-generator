7f059d94cbd7cada8a6f1f18a70384e0b5a29626
Henrique Mendes de Freitas Mariano <henriquiui70@gmail.com>
2022-08-10 00:08:27 -0300

    Add: nft-art-generator module

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
 const sharp = require('sharp');
+const mergeImages = require('merge-images');
+const { Canvas, Image } = require('canvas');
+// import mergeImages from 'merge-images';
 
 const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -29,10 +32,10 @@ function randElement(arr) {
 function getRandomName() {
     const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
     const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
+
     const randAdj = randElement(adjectives);
     const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
+    const name = `${randAdj}-${randName}`;
 
 
     if (takenNames[name] || !name) {
@@ -43,7 +46,7 @@ function getRandomName() {
     }
 }
 
-function getLayer(name, skip=0.0) {
+function getLayer(name, skip = 0.0) {
     const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
     const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
     const layer = svg.match(re)[0];
@@ -65,7 +68,7 @@ function createImage(idx) {
     const bg = randInt(5);
     const hair = randInt(7);
     const eyes = randInt(9);
-    const nose = randInt(4); 
+    const nose = randInt(4);
     const mouth = randInt(5);
     const beard = randInt(3);
     // 18,900 combinations
@@ -93,7 +96,7 @@ function createImage(idx) {
             description: `A drawing of ${name.split('-').join(' ')}`,
             image: `${idx}.png`,
             attributes: [
-                { 
+                {
                     beard: '',
                     rarity: 0.5
                 }
@@ -109,15 +112,20 @@ function createImage(idx) {
 
 
 // Create dir if not exists
-if (!existsSync('./out')){
-    mkdirSync('./out');
-}
+// if (!existsSync('./out')){
+//     mkdirSync('./out');
+// }
 
-// Cleanup dir before each run
-readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
+// // Cleanup dir before each run
+// readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
 
+mergeImages(['/pngs/mapas/samambaia02.png', '/pngs/mapas/samambaia03.png', '/pngs/mapas/varjao01.png', '/pngs/mapas/varjao02.png'], {
+    Canvas: Canvas,
+    Image: Image
+})
+    .then(b64 => document.querySelector('img').src = b64);
 
-do {
-    createImage(idx);
-    idx--;
-  } while (idx >= 0);
+// do {
+//     createImage(idx);
+//     idx--;
+//   } while (idx >= 0);
